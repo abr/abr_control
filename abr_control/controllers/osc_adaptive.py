5ccc141b2f6a11caded5f20bc35d191c8e323587
travis dewolf <travis.dewolf@gmail.com>
2016-08-25 08:44:28 -0400

    Modified to use robot_config, set to always track
    - also removed licence things at the top, we probably want to use
      something other than the MIT licence

--- abr_control/controllers/osc_adaptive.py
+++ abr_control/controllers/osc_adaptive.py
@@ -1,19 +1,3 @@
-'''
-Copyright (C) 2016 Travis DeWolf
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
 import numpy as np
 import time
 
@@ -59,29 +43,17 @@ class controller(osc.controller):
     adaptation using a Nengo model
     """
 
-    # # for scaling and biasing input to -1 to 1 range
-    # q_bias = np.array([2.66e-03, 1.07, -1.93, 7.37e-01, 2.54, 1.27e+01])
-    # q_max_vals = np.array([0.99, 1.63, -1.40, 1.64, 3.368, 14.27])
-    # q_min_vals = np.array([-1.74, 0.67, -2.54, -0.68, 1.35, 11.78])
-    # q_scale = (q_max_vals - q_min_vals) / 2
-    # # for scaling and biasing input to -1 to 1 range
-    # dq_bias = np.array([-0.03, 0.20, -0.08, -0.30, 0.51, -0.44])
-    # dq_max_vals = np.array([25.44, 7.56, 4.29, 8.62, 15.68, 8.72])
-    # dq_min_vals = np.array([-22.74, -3.09, -6.27, -9.65, -13.57, -20.53])
-    # dq_scale = (dq_max_vals - dq_min_vals) / 2
-
-    def __init__(self, robot_config, tracking=False, learning_rate=1e-3):
+    def __init__(self, robot_config, learning_rate=1e-3):
         """
         learning_rate float: controls the speed of neural adaptation
         """
 
-        super(controller, self).__init__(robot_config, tracking)
+        super(controller, self).__init__(robot_config)
 
         self.u_adapt = np.zeros(self.robot_config.num_joints)
 
-        if self.tracking is True:
-            self.track_u_adapt = []
-            self.track_adapt_input = []
+        self.track_u_adapt = []
+        self.track_adapt_input = []
 
         dim = self.robot_config.num_joints
         nengo_model = nengo.Network()
@@ -90,11 +62,8 @@ class controller(osc.controller):
             def qdq_input(t):
                 """ returns q and dq scaled and bias to
                 be around -1 to 1 """
-                # vals = np.hstack([(self.q - self.q_bias) / self.q_scale,
-                #                   (self.dq - self.dq_bias) / self.dq_scale])
-                # self.track_adapt_input.append(np.copy(vals))
-                # return vals
-                return np.hstack([self.q, self.dq])
+                return self.robot_config.scaledown(
+                    'CB', np.hstack([self.q, self.dq]))
             qdq_input = nengo.Node(qdq_input, size_out=dim*2)
 
             def u_input(t):
@@ -115,22 +84,20 @@ class controller(osc.controller):
                                        seed=10)
 
             nengo.Connection(qdq_input, adapt_ens)
-            learn_conn = nengo.Connection(
-                adapt_ens, output,
-                # start with outputting just zero
-                function=lambda x: np.zeros(dim),
-                learning_rule_type=nengo.PES(learning_rate),)
-                # use the weights solver that lets you keep
-                # learning from the what's saved to file
-                # solver=KeepLearningSolver('weights.npz'))
-            nengo.Connection(
-                u_input, learn_conn.learning_rule,
-                # invert because we're providing error not reward
-                transform=-1, synapse=.01)
+            learn_conn = \
+                nengo.Connection(adapt_ens, output,
+                                 # start with outputting just zero
+                                 function=lambda x: np.zeros(dim),
+                                 learning_rule_type=nengo.PES(learning_rate),
+                                 # use the weights solver that lets you keep
+                                 # learning from the what's saved to file
+                                 solver=KeepLearningSolver('weights.npz'))
+            nengo.Connection(u_input, learn_conn.learning_rule,
+                             # invert because we're providing error not reward
+                             transform=-1, synapse=.01)
 
-            self.probe_weights = nengo.Probe(
-                learn_conn, 'weights',
-                sample_every=.1)  # in seconds
+            self.probe_weights = nengo.Probe(learn_conn, 'weights',
+                                             sample_every=.1)  # in seconds
 
         self.sim = nengo.Simulator(nengo_model)
 
@@ -150,8 +117,7 @@ class controller(osc.controller):
         # run the simulation to generate the adaptive signal
         self.sim.run(time_in_seconds=.001, progress_bar=False)
 
-        if self.tracking is True:
-            self.track_u_adapt.append(np.copy(self.u_adapt))
+        self.track_u_adapt.append(np.copy(self.u_adapt))
         self.u += self.u_adapt
 
         print('u_adapt: ', self.u_adapt)
--- abr_control/controllers/reach.py
+++ abr_control/controllers/reach.py
@@ -1,19 +1,3 @@
-'''
-Copyright (C) 2016 Travis DeWolf
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
 import numpy as np
 
 try:
@@ -26,7 +10,7 @@ class controller:
     """ Implements an operational space controller (OSC)
     """
 
-    def __init__(self, robot_config, tracking=False):  # noqa C901
+    def __init__(self, robot_config):  # noqa C901
 
         self.robot_config = robot_config
 
@@ -35,10 +19,8 @@ class controller:
 
         self.target = np.zeros(3)
 
-        self.tracking = tracking
-        if self.tracking is True:
-            self.track_u = []
-            self.track_u_adapt = []
+        self.track_u = []
+        self.track_u_adapt = []
 
         dim = self.robot_config.num_joints
         self.model = nengo.Network('REACH', seed=5)
@@ -203,8 +185,7 @@ class controller:
         # run the simulation to generate the control signal
         self.sim.run(time_in_seconds=.005, progress_bar=False)
 
-        if self.tracking is True:
-            self.track_u.append(np.copy(self.u))
+        self.track_u.append(np.copy(self.u))
 
         # return the sum of the two
         return self.u
