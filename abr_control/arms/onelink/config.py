2f3de23e583d974fe836caba6cb0228edb5bb12a
travis dewolf <travis.dewolf@gmail.com>
2016-08-25 11:08:24 -0400

    Reworking licences

--- abr_control/arms/onelink/config.py
+++ abr_control/arms/onelink/config.py
@@ -1,20 +1,3 @@
-'''
-Copyright (C) 2016 Travis DeWolf
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
 import cloudpickle
 import os
 import numpy as np
@@ -24,7 +7,7 @@ from .. import robot_config
 
 
 class robot_config(robot_config.robot_config):
-    """ robot config file for the onelink arm """
+    """ Robot config file for the onelink arm """
 
     def __init__(self):
 
