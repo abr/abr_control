ff49c1a728b0ec63a3d761afb175d0e31e60a820
travis dewolf <travis.dewolf@gmail.com>
2016-11-15 14:45:35 -0500

    added ability to specify x inside reference frame for transforms and jacobians

--- abr_control/controllers/osc_obstacles.py
+++ abr_control/controllers/osc_obstacles.py
@@ -34,108 +34,123 @@ class controller:
         # calculate the Jacobian for the end effector
         JEE = self.robot_config.J('EE', q)
 
-        # # calculate the inertia matrix in joint space
-        # Mq = self.robot_config.Mq(q)
-        #
-        # # calculate the effect of gravity in joint space
-        # Mq_g = self.robot_config.Mq_g(q)
-        #
-        # # convert the mass compensation into end effector space
-        # Mx_inv = np.dot(JEE, np.dot(np.linalg.pinv(Mq), JEE.T))
-        # svd_u, svd_s, svd_v = np.linalg.svd(Mx_inv)
-        # # cut off any singular values that could cause control problems
-        # singularity_thresh = .00025
-        # for ii in range(len(svd_s)):
-        #     svd_s[ii] = 0 if svd_s[ii] < singularity_thresh else \
-        #         1./float(svd_s[ii])
-        # # numpy returns U,S,V.T, so have to transpose both here
-        # Mx = np.dot(svd_v.T, np.dot(np.diag(svd_s), svd_u.T))
-        #
-        # # calculate desired force in (x,y,z) space
-        # dx = np.dot(JEE, dq)
-        # # implement velocity limiting
-        # lamb = self.kp / self.kv
-        # x_tilde = xyz - target_state[:3]
-        # sat = self.vmax / (lamb * np.abs(x_tilde))
-        # scale = np.ones(3)
-        # if np.any(sat < 1):
-        #     index = np.argmin(sat)
-        #     unclipped = self.kp * x_tilde[index]
-        #     clipped = self.kv * self.vmax * np.sign(x_tilde[index])
-        #     scale = np.ones(3) * clipped / unclipped
-        #     scale[index] = 1
-        # u_xyz = -self.kv * (dx - target_state[3:] -
-        #                     np.clip(sat / scale, 0, 1) *
-        #                     -lamb * scale * x_tilde)
-        # u_xyz = np.dot(Mx, u_xyz)
-        #
-        # self.training_signal = np.dot(JEE.T, u_xyz)
-        # # add in gravity compensation, not included in training signal
-        # u = self.training_signal - Mq_g
-        #
-        # # calculate the null space filter
-        # Jdyn_inv = np.dot(Mx, np.dot(JEE, np.linalg.inv(Mq)))
-        # null_filter = (np.eye(self.robot_config.num_joints) -
-        #                np.dot(JEE.T, Jdyn_inv))
-        #
-        # q_des = np.zeros(self.robot_config.num_joints)
-        # dq_des = np.zeros(self.robot_config.num_joints)
-        #
-        # # calculated desired joint angle acceleration using rest angles
-        # for ii in range(1, self.robot_config.num_joints):
-        #     if self.robot_config.rest_angles[ii] is not None:
-        #         q_des[ii] = (
-        #             ((self.robot_config.rest_angles[ii] - q[ii]) + np.pi) %
-        #              (np.pi*2) - np.pi)
-        #         dq_des[ii] = dq[ii]
-        # # only compensate for velocity for joints with a control signal
-        # nkp = self.kp * .1
-        # nkv = np.sqrt(nkp)
-        # u_null = np.dot(Mq, (nkp * q_des - nkv * dq_des))
-        #
-        # u += np.dot(null_filter, u_null)
+        # calculate the inertia matrix in joint space
+        Mq = self.robot_config.Mq(q)
 
-        dx  = np.dot(JEE, dq)
+        # calculate the effect of gravity in joint space
+        Mq_g = self.robot_config.Mq_g(q)
+
+        # convert the mass compensation into end effector space
+        Mx_inv = np.dot(JEE, np.dot(np.linalg.pinv(Mq), JEE.T))
+        svd_u, svd_s, svd_v = np.linalg.svd(Mx_inv)
+        # cut off any singular values that could cause control problems
+        singularity_thresh = .00025
+        for ii in range(len(svd_s)):
+            svd_s[ii] = 0 if svd_s[ii] < singularity_thresh else \
+                1./float(svd_s[ii])
+        # numpy returns U,S,V.T, so have to transpose both here
+        Mx = np.dot(svd_v.T, np.dot(np.diag(svd_s), svd_u.T))
+
+        # calculate desired force in (x,y,z) space
+        dx = np.dot(JEE, dq)
+        # implement velocity limiting
+        lamb = self.kp / self.kv
         x_tilde = xyz - target_state[:3]
-        u_xyz = -self.kv * (dx - target_state[3:]) - self.kp * x_tilde
-        u = np.dot(np.linalg.pinv(JEE), u_xyz)
+        sat = self.vmax / (lamb * np.abs(x_tilde))
+        scale = np.ones(3)
+        if np.any(sat < 1):
+            index = np.argmin(sat)
+            unclipped = self.kp * x_tilde[index]
+            clipped = self.kv * self.vmax * np.sign(x_tilde[index])
+            scale = np.ones(3) * clipped / unclipped
+            scale[index] = 1
+        u_xyz = -self.kv * (dx - target_state[3:] -
+                            np.clip(sat / scale, 0, 1) *
+                            -lamb * scale * x_tilde)
+        u_xyz = np.dot(Mx, u_xyz)
+
+        self.training_signal = np.dot(JEE.T, u_xyz)
+        # add in gravity compensation, not included in training signal
+        u = self.training_signal - Mq_g
+
+        # calculate the null space filter
+        Jdyn_inv = np.dot(Mx, np.dot(JEE, np.linalg.inv(Mq)))
+        null_filter = (np.eye(self.robot_config.num_joints) -
+                       np.dot(JEE.T, Jdyn_inv))
+
+        q_des = np.zeros(self.robot_config.num_joints)
+        dq_des = np.zeros(self.robot_config.num_joints)
+
+        # calculated desired joint angle acceleration using rest angles
+        for ii in range(1, self.robot_config.num_joints):
+            if self.robot_config.rest_angles[ii] is not None:
+                q_des[ii] = (
+                    ((self.robot_config.rest_angles[ii] - q[ii]) + np.pi) %
+                     (np.pi*2) - np.pi)
+                dq_des[ii] = dq[ii]
+        # only compensate for velocity for joints with a control signal
+        nkp = self.kp * .1
+        nkv = np.sqrt(nkp)
+        u_null = np.dot(Mq, (nkp * q_des - nkv * dq_des))
+
+        u += np.dot(null_filter, u_null)
 
         # add in obstacles avoidance
-        points_of_interest = []
-        for ii in range(1, self.robot_config.num_joints):
-            points_of_interest.append('joint%i' % ii)
-        for ii in range(self.robot_config.num_links):
-            points_of_interest.append('link%i' % ii)
-        points_of_interest.append('EE')
         for obstacle in self.obstacles:
-            # for each point of interest
-            for poi in points_of_interest:
-                # calculate distance to obstacle
-                xyz = self.robot_config.T(poi, q)
-                dist = (np.sqrt(np.sum((np.array(obstacle[:3] - xyz))**2)) -
-                            obstacle[3])
-                if dist < self.threshold:
-                    # apply force back in opposite direction
-                    # scaled by our object distance function
-                    force_xyz = (xyz - obstacle[:3]) #* self.dist_func(dist)
-                    # get Jacbian to transform into joint torques
-                    J = self.robot_config.J(poi, q)[:3]
-                    # add to the control signal
-                    # u_obstacle = np.dot(np.linalg.pinv(J), force_xyz)
-                    JEE_inv = np.linalg.pinv(JEE)
-                    filter1 = np.eye(3) - np.dot(JEE_inv, JEE)
-                    u_obstacle = np.dot(
-                        np.linalg.pinv(np.dot(J, filter1)),
-                        (force_xyz - np.dot(
-                            J,
-                            np.dot(JEE_inv, x_tilde))))
+            # our vertex of interest is the center point of the obstacle
+            v = np.array(obstacle[:3])
 
-                    print('dist %s: %.3f' % (poi, dist))
-                    print('u_obs: ', [float('%.3f' % val) for val in u_obstacle])
-                    if dist < .01:
-                        u = u_obstacle
-                    else:
-                        u += u_obstacle
+            # TODO: INCORPORATE THE RADIUS OF THE OBSTACLE
+
+            # find the closest point of each link to the obstacle
+            for ii in range(self.robot_config.num_links - 1):
+                # using notation from (Khatib, 1986)
+                m1 = self.robot_config.T('joint%i' % ii, q)
+                if ii == self.robot_config.num_links - 1:
+                    m2 = self.robot_config.T('EE', q)
+                else:
+                    m2 = self.robot_config.T('joint%i' % (ii + 1), q)
+
+                # TODO: NEED TO TRANSFORM UPPER AND LOWER XYZ
+                # INTO LINK ii's REFERENCE FRAME
+
+                # find the nearest point on this segment (eq A1-1)
+                l = self.robot_config.L[ii]
+                lamb = np.dot(v * m1, m1 * m2) / l**2
+
+                # calculate m and distance to the vertex (eq A1-2)
+                if 0 < lamb and lamb < 1:
+                    m = m1 + lamb * (m2 - m1)
+                    ro = np.sqrt(ro1**2 - lamb**2 * l**2)
+                elif lamb < 0:
+                    m = m1
+                    ro = ro1
+                elif lamb > 1:
+                    m = m2
+                    ro = ro2
+
+                drodx = m / ro
+
+                if ro < self.threshold:
+                    Fpsp = (eta * (1.0/ro - 1.0/self.threshold) *
+                            1.0/ro**2 * drodx)
+
+                    # Jpsp = 
+
+                    # convert the mass compensation into
+                    # point subjected to the potential space
+                    Mxpsp_inv = np.dot(Jpsp,
+                                       np.dot(np.linalg.pinv(Mq), Jpsp.T))
+                    svd_u, svd_s, svd_v = np.linalg.svd(Mxpsp_inv)
+                    # cut off singular values that could cause problems
+                    singularity_thresh = .00025
+                    for ii in range(len(svd_s)):
+                        svd_s[ii] = 0 if svd_s[ii] < singularity_thresh else \
+                            1./float(svd_s[ii])
+                    # numpy returns U,S,V.T, so have to transpose both here
+                    Mxpsp = np.dot(svd_v.T, np.dot(np.diag(svd_s), svd_u.T))
+
+                    u += np.dot(Jpsp.T, np.dot(Mxpsp, Fpsp))
 
         return u
 
