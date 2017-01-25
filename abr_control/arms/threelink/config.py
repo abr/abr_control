more orientation work, fixed Jacobian
- also added weight of joints (instead of just links) into Mq and gravity calculations

abr_control/arms/threelink/config.py
@@ -12,6 +12,8 @@ class robot_config(robot_config.robot_config):
         super(robot_config, self).__init__(num_joints=3, num_links=3,
                                            robot_name='threelink', **kwargs)
 
+        self._T = {}  # dictionary for storing calculated transforms
+
         # for the null space controller, keep arm near these angles
         self.rest_angles = np.array([np.pi/4.0, np.pi/4.0, np.pi/4.0],
                                     dtype='float32')
@@ -43,6 +45,7 @@ class robot_config(robot_config.robot_config):
         self.L_com = self.L / 2.0
 
         # ---- Joint Transform Matrices ----
+        # TODO: Rework these to be in same form as UR5 and Jaco2
 
         # Transform matrix : origin -> joint 0
         self.Torg0 = sp.Matrix([
@@ -104,10 +107,12 @@ class robot_config(robot_config.robot_config):
             [0, 0, 0, 1]])
 
         # orientation part of the Jacobian
-        # accounting for which axis is rotated around at each joint
-        self.J_orientation = [[0.0, 0.0, 1.0],  # joint 0 rotates around z axis
-                              [0.0, 0.0, 1.0],  # joint 1 rotates around z axis
-                              [0.0, 0.0, 1.0]]  # joint 2 rotates around z axis
+        kz = sp.Matrix([0, 0, 1])  # all joints rotate around their z axis
+        self.J_orientation = [
+            self._calc_T('joint0')[:3, :3] * kz,  # joint 0 angular velocity
+            self._calc_T('joint1')[:3, :3] * kz,  # joint 1 angular velocity
+            self._calc_T('joint2')[:3, :3] * kz]  # joint 2 angular velocity
+
 
     def _calc_T(self, name):
         """ Uses Sympy to generate the transform for a joint or link
@@ -115,28 +120,24 @@ class robot_config(robot_config.robot_config):
         name string: name of the joint or link, or end-effector
         """
 
-        # ---- Joint Transforms ----
+        if self._T.get(name, None) is None:
+            if name == 'joint0':
+                self._T[name] = self.Torg0
+            elif name == 'link0':
+                self._T[name] = self.Tlorg0
+            elif name == 'joint1':
+                self._T[name] = self.Torg0 * self.T01
+            elif name == 'link1':
+                self._T[name] = self.Torg0 * self.Tl01
+            elif name == 'joint2':
+                self._T[name] = self.Torg0 * self.T01 * self.T12
+            elif name == 'link2':
+                self._T[name] = self.Torg0 * self.T01 * self.Tl12
+            elif name == 'link3':
+                self._T[name] = self.Torg0 * self.T01 * self.T12 * self.Tl23
+            elif name == 'EE':
+                self._T[name] = self.Torg0 * self.T01 * self.T12 * self.T2EE
+            else:
+                raise Exception('Invalid transformation name: %s' % name)
 
-        if name == 'joint0':
-            T = self.Torg0
-        elif name == 'joint1':
-            T = self.Torg0 * self.T01
-        elif name == 'joint2':
-            T = self.Torg0 * self.T01 * self.T12
-        elif name == 'EE':
-            T = self.Torg0 * self.T01 * self.T12 * self.T2EE
-
-        # ---- COM Transforms ----
-
-        elif name == 'link0':
-            T = self.Tlorg0
-        elif name == 'link1':
-            T = self.Torg0 * self.Tl01
-        elif name == 'link2':
-            T = self.Torg0 * self.T01 * self.Tl12
-        elif name == 'link3':
-            T = self.Torg0 * self.T01 * self.T12 * self.Tl23
-        else:
-            raise Exception('Invalid transformation name: %s' % name)
-
-        return T
+        return self._T[name]

examples/ur5/reaching.py
@@ -4,16 +4,18 @@ The simulation ends after 1.5 simulated seconds, and the
 trajectory of the end-effector is plotted in 3D.
 """
 import numpy as np
+import signal
+import sys
 
 import abr_control
 
 # initialize our robot config for the ur5
 robot_config = abr_control.arms.ur5.config(
-    regenerate_functions=False)
+    regenerate_functions=True)
 
 # instantiate controller
 ctrlr = abr_control.controllers.osc(
-    robot_config, kp=200, vmax=0.5)
+    robot_config, kp=20, vmax=0.5)
 
 # create our VREP interface
 interface = abr_control.interfaces.vrep(
@@ -23,6 +25,37 @@ interface.connect()
 # set up lists for tracking data
 ee_track = []
 target_track = []
+
+def on_exit(signal, frame):
+    """ A function for plotting the end-effector trajectory and error """
+    global ee_track, target_track
+    ee_track = np.array(ee_track)
+    target_track = np.array(target_track)
+
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # plot start point of hand
+    ax.plot(*ee_track, 'bx', mew=10)
+    # plot trajectory of hand
+    ax.plot(*ee_track[:, 0])
+    # plot trajectory of target
+    ax.plot(*target_track, 'rx', mew=10)
+    ax.set_xlim3d(-1, 1)
+    ax.set_ylim3d(-1, 1)
+    ax.set_zlim3d(0, 1.5)
+
+    plt.figure()
+    plt.plot(np.sqrt(np.sum((np.array(target_track) -
+                             np.array(ee_track))**2, axis=1)))
+    plt.ylabel('Error (m)')
+    plt.xlabel('Time (ms)')
+    plt.show()
+    sys.exit()
+
+# call on_exit when ctrl-c is pressed
+signal.signal(signal.SIGINT, on_exit)
 
 try:
     num_targets = 0
@@ -62,36 +95,3 @@ try:
 finally:
     # stop and reset the VREP simulation
     interface.disconnect()
-
-    ee_track = np.array(ee_track)
-    target_track = np.array(target_track)
-
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-
-    # plot start point of hand
-    ax.plot([ee_track[0, 0]],
-            [ee_track[0, 1]],
-            [ee_track[0, 2]],
-            'bx', mew=10)
-    # plot trajectory of hand
-    ax.plot(ee_track[:, 0],
-            ee_track[:, 1],
-            ee_track[:, 2])
-    # plot trajectory of target
-    ax.plot(target_track[:, 0],
-            target_track[:, 1],
-            target_track[:, 2],
-            'rx', mew=10)
-    ax.set_xlim3d(-1, 1)
-    ax.set_ylim3d(-1, 1)
-    ax.set_zlim3d(0, 1.5)
-
-    plt.figure()
-    plt.plot(np.sqrt(np.sum((np.array(target_track) -
-                             np.array(ee_track))**2, axis=1)))
-
-    plt.ylabel('Error (m)')
-    plt.xlabel('Time (ms)')
-    plt.show()