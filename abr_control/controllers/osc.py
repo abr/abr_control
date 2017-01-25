more orientation work, hopefully fixed Jacobian component

abr_control/controllers/osc.py
@@ -21,7 +21,7 @@ class controller:
 
     def control(self, q, dq,
                 target_x, target_dx=np.zeros(3),
-                target_w=None, target_dw=None, w_axes='sxyz',
+                target_quat=None, target_w=np.zeros(3),
                 mask=[1, 1, 1, 0, 0, 0],
                 ref_frame='EE', offset=[0, 0, 0]):
         """ Generates the control signal
@@ -30,13 +30,10 @@ class controller:
         dq np.array: the current joint velocities
         target_x np.array: the target end-effector position values, post-mask
         target_dx np.array: the target end-effector velocity, post-mask
-        target_w np.array: the target Euler angles
-        target_dw np.array: the target angular velocities
-        w_axes string: the order of rotation of the Euler angles with
-                       rotation matrices. Default matches VREP.
-                       First character is either s for static or
-                       r for relative.
-        mask list: indicates the [x, y, z, alpha, beta, gamma] values
+        target_quat np.array: the target orientation as a quaternion
+                              in the form [w, x, y, z]
+        target_w np.array: the target angular velocities
+        mask list: indicates the [x, y, z, roll, pitch, yaw] values
                    to be controlled, 1 = control, 0 = ignore
         ref_frame string: the frame of reference of control point
         offset list: point of interest from the frame of reference
@@ -47,11 +44,10 @@ class controller:
         # calculate the Jacobian for the end effector
         JEE = self.robot_config.J(ref_frame, q, x=offset)
 
-        # apply mask to isolate DOFs of interest
-        JEE = np.array([JEE[ii] for ii in range(6) if mask[ii] == 1])
-
-        # calculate the end-effector velocity information
-        dx = np.dot(JEE, dq)
+        # calculate the end-effector linear and angular velocity
+        full_velocity_signal = np.dot(JEE, dq)
+        dx = full_velocity_signal[:3]  # linear velocity
+        w = full_velocity_signal[3:]  # angular velocity
 
         # calculate the inertia matrix in joint space
         Mq = self.robot_config.Mq(q)
@@ -59,6 +55,8 @@ class controller:
         # calculate the effect of gravity in joint space
         Mq_g = self.robot_config.Mq_g(q)
 
+        # apply mask to Jacobian before
+        JEE *= np.array(mask).reshape(6, 1)
         # calculate the inertia matrix in task space
         Mq_inv = np.linalg.inv(Mq)
         JEE_Mq_inv = np.dot(JEE, Mq_inv)  # save for use again below
@@ -67,60 +65,61 @@ class controller:
         # is slightly faster than doing it manually with svd
         Mx = np.linalg.pinv(Mx_inv, rcond=.01)
 
-        # calculate desired force in (x,y,z) space
-        if self.vmax is not None:
-            # implement velocity limiting
+        u_task = np.zeros(6)  # task space control signal before masking
+
+        # generate the position control signal in task space if a target
+        # position was provided, and the mask includes positions
+        if target_x is not None and np.sum(mask[:3]) > 0:
+            # calculate the position error
             x_tilde = xyz - target_x
-            sat = self.vmax / (self.lamb * np.abs(x_tilde))
-            if np.any(sat < 1):
-                index = np.argmin(sat)
-                unclipped = self.kp * x_tilde[index]
-                clipped = self.kv * self.vmax * np.sign(x_tilde[index])
-                scale = np.ones(3, dtype='float32') * clipped / unclipped
-                scale[index] = 1
+
+            if self.vmax is not None:
+                # implement velocity limiting
+                sat = self.vmax / (self.lamb * np.abs(x_tilde))
+                if np.any(sat < 1):
+                    index = np.argmin(sat)
+                    unclipped = self.kp * x_tilde[index]
+                    clipped = self.kv * self.vmax * np.sign(x_tilde[index])
+                    scale = np.ones(3, dtype='float32') * clipped / unclipped
+                    scale[index] = 1
+                else:
+                    scale = np.ones(3, dtype='float32')
+
+                u_task[:3] = -self.kv * (dx - target_dx -
+                                    np.clip(sat / scale, 0, 1) *
+                                    -self.lamb * scale * x_tilde)
             else:
-                scale = np.ones(3, dtype='float32')
+                # generate (x,y,z) force without velocity limiting)
+                u_task[:3] = -self.kp * x_tilde + self.kv * (target_dx - dx)
 
-            u_xyz = -self.kv * (dx - target_dx -
-                                np.clip(sat / scale, 0, 1) *
-                                -self.lamb * scale * x_tilde)
-        else:
-            # generate (x,y,z) force without velocity limiting)
-            u_xyz = (self.kp * (target_x - xyz)  +
-                     self.kv * (target_dx - dx))
-        print('u_xyz: ', u_xyz)
+        # generate the orientation control signal in task space if a target
+        # orientation was provided, and the mask includes orientation angles
+        if target_quat is not None and np.sum(mask[3:]) > 0:
+            # get the quaternion describing orientation of ref_frame
+            quat = self.robot_config.orientation(ref_frame, q)
 
-        u_xyz = np.dot(Mx, u_xyz)
+            # calculate the error between the two quaternions as
+            # described in (Nakanishi et al, 2008)
+            target_e = np.array([
+                [0, -target_quat[3], target_quat[2]],
+                [target_quat[3], 0, -target_quat[1]],
+                [-target_quat[2], target_quat[1], 0]])
+            error_quat = (target_quat[0] * quat[1:] -
+                          quat[0] * target_quat[1:] +
+                          np.dot(target_e, quat[1:]))
 
-        # if target_w is not None:
-        #     # if a target orientation was provided, generate control
-        #     # signal for the orientation of the ref_frame
-        #     target_dw = np.zeros(3) if target_dw is None else target_dw
-        #
-        #     # convert target_w into a quaternion
-        #     print('target_w: ', target_w)
-        #     target_quat = (
-        #         abr_control.utils.transformations.quaternion_from_euler(
-        #             target_w[0], target_w[1], target_w[2], axes=w_axes))
-        #
-        #     # get the quaternion describing orientation of ref_frame
-        #     quat = self.robot_config.orientation(ref_frame, q)
-        #
-        #     # calculate the error between the two quaternions as
-        #     # described in (Nakanishi et al, 2008)
-        #     target_e = np.array([
-        #         [0, -target_quat[3], target_quat[2]],
-        #         [target_quat[3], 0, -target_quat[1]],
-        #         [-target_quat[2], target_quat[1], 0]])
-        #     error_quat = (target_quat[0] * quat[1:] -
-        #                   quat[0] * target_quat[1:] +
-        #                   np.dot(target_e, quat[1:]))
+            ko = 300
+            ka = np.sqrt(ko)
+            u_task[3:] = (ka * (target_w - w) - ko * error_quat)
+        # apply mask
+        u_task *= mask
 
-        print('Mq_g: ', Mq_g)
+        # incorporate task space inertia matrix
+        u_task = np.dot(Mx, u_task)
 
         # TODO: This is really awkward, but how else to get out
         # this signal for dynamics adaptation training?
-        self.training_signal = np.dot(JEE.T, u_xyz)
+        self.training_signal = np.dot(JEE.T, u_task)
         # add in gravity compensation, not included in training signal
         u = self.training_signal - Mq_g
 