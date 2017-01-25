more orientation work, fixed Jacobian
- also added weight of joints (instead of just links) into Mq and gravity calculations

examples/ur5/reaching_control_orientation.py
@@ -12,7 +12,7 @@ import abr_control
 # initialize our robot config for the ur5
 robot_config = abr_control.arms.ur5.config(regenerate_functions=True)
 # instantiate controller
-ctrlr = abr_control.controllers.osc(robot_config, kp=200, vmax=None)
+ctrlr = abr_control.controllers.osc(robot_config, kp=1000, vmax=None)
 # create our VREP interface
 interface = abr_control.interfaces.vrep(robot_config, dt=.001)
 interface.connect()
@@ -28,13 +28,14 @@ def on_exit(signal, frame):
     target_track = np.array(target_track)
 
     import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
     # plot start point of hand
-    ax.plot(*ee_track, 'bx', mew=10)
+    ax.plot(*ee_track[0], 'bx', mew=10)
     # plot trajectory of hand
-    ax.plot(*ee_track[:, 0])
+    ax.plot(*ee_track)
     # plot trajectory of target
     ax.plot(*target_track, 'rx', mew=10)
     ax.set_xlim3d(-1, 1)
@@ -63,9 +64,11 @@ try:
     interface.set_xyz(name='target', xyz=target_xyz)
     # get the target orientation to match
     target_angles = interface.get_orientation(name='target')
+    target_quat = abr_control.utils.transformations.quaternion_from_euler(
+        target_angles[0], target_angles[1], target_angles[2], 'rxyz')
 
     count = 0.0
-    while count < 1500:
+    while 1:
         # get arm feedback from VREP
         feedback = interface.get_feedback()
 
@@ -73,17 +76,23 @@ try:
         u = ctrlr.control(
             q=feedback['q'],
             dq=feedback['dq'],
-            # target_x=[target_xyz[0], target_xyz[1], 0],
             target_x=target_xyz,
-            target_dx=[0, 0, 0],
-            # target_w=target_angles,
-            # mask=[1, 0, 1, 0, 1, 0])
-            mask=[1, 1, 1, 0, 0, 0])
+            target_quat=target_quat,
+            mask=[0, 0, 0, 1, 1, 1])
 
         # apply the control signal, step the sim forward
         interface.apply_u(u)
 
         ee_xyz = interface.get_xyz('hand')
+        angles = interface.get_orientation('UR5_link6')
+        quat_from_vrep = abr_control.utils.transformations.quaternion_from_euler(
+            angles[0], angles[1], angles[2], 'rxyz')
+        print('quat_from_vrep: ', quat_from_vrep)
+        print('quat from config: ', robot_config.orientation('EE', feedback['q']))
+        print('target quat: ', target_quat)
+        # print('orientation: ', angles)
+        # print('target orientation: ', target_angles)
+        interface.set_orientation('hand', angles)
         print('error: ', np.sqrt(np.sum((target_xyz - ee_xyz)**2)))
         # track data
         ee_track.append(np.copy(ee_xyz))
@@ -95,3 +104,4 @@ finally:
     # stop and reset the VREP simulation
     interface.disconnect()
 
+on_exit()