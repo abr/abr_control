a726c5ddc64ad8ab0ccd8d311490ce6daa6fbd43
travis dewolf <travis.dewolf@gmail.com>
2017-01-26 12:09:41 -0500

    added float controller

--- abr_control/controllers/__init__.py
+++ abr_control/controllers/__init__.py
@@ -1,3 +1,5 @@
+from .floating import controller as floating
+from .dynamic_floating import controller as dynamic_floating
 from .osc import controller as osc
 from .trajectory import controller as trajectory
 
