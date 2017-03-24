a2773b0169e8c5952872e4636c24168319194685
travis dewolf <travis.dewolf@gmail.com>
2017-03-24 17:20:31 -0400

    cleaned up dynamics_adaptation, osc optimization
    - also removed quaternions util
    - added parameters for using C and dJ
    - sped up the null control signal calculation

--- abr_control/controllers/osc.py
+++ abr_control/controllers/osc.py
@@ -6,19 +6,33 @@ class controller:
     """
 
     def __init__(self, robot_config, kp=1, kv=None, vmax=0.5,
-                 null_control=True):
+                 null_control=True, use_C=False, use_dJ=False):
+        """
+        kp float: the position gain term
+        kv float: the velocity gain term
+        vmax float: the max allowed velocity of the end-effector
+            if the control signal specifies something above this
+            value it is clipped if set to None no clipping occurs
+        null_control boolean: apply a secondary control signal which
+            drives the arm to specified resting joint angles without
+            affecting the movement of the end-effector
+        use_C boolean: calculate and compensate for the Coriolis and
+            centripetal effects of the arm
+        use_dJ boolean: use the Jacobian derivative to estimate and
+            cancel out the current acceleration
+        """
 
         self.robot_config = robot_config
-
-        # proportional gain term
         self.kp = kp
-        # derivative gain term
         self.kv = np.sqrt(self.kp) if kv is None else kv
-        # velocity limit of the end-effector
         self.vmax = vmax
         self.lamb = self.kp / self.kv
-
         self.null_control = null_control
+        self.use_C = use_C
+        self.use_dJ = use_dJ
+
+        self.null_indices = ~np.isnan(self.robot_config.rest_angles)
+        self.dq_des = np.zeros(self.robot_config.num_joints)
 
     def control(self, q, dq,
                 target_pos, target_vel=np.zeros(3),
@@ -49,8 +63,6 @@ class controller:
 
         # calculate the end-effector linear and angular velocity
         full_velocity_signal = np.dot(J, dq)
-        dx = full_velocity_signal[:3]  # linear velocity
-        # w = full_velocity_signal[3:]  # angular velocity
 
         # calculate the inertia matrix in joint space
         M = self.robot_config.M(q)
@@ -59,10 +71,12 @@ class controller:
         # J *= np.array(mask).reshape(6, 1)
         # calculate the inertia matrix in task space
         M_inv = np.linalg.inv(M)
+        # calculate the Jacobian for end-effector with no offset
         JEE = self.robot_config.J(ref_frame, q)[:3]
         Mx_inv = np.dot(JEE, np.dot(M_inv, JEE.T))
         # using the rcond to set singular values < thresh to 0
         # is slightly faster than doing it manually with svd
+        # singular values < (rcond * max(singular_values)) set to 0
         Mx = np.linalg.pinv(Mx_inv, rcond=.01)
 
         u_task = np.zeros(3)  # task space control signal before masking
@@ -72,7 +86,8 @@ class controller:
         if target_pos is not None and np.sum(mask[:3]) > 0:
             # calculate the position error
             x_tilde = np.array(xyz - target_pos)
-            # x_tilde = np.array([0, 0, 0.05])
+            # isolate the linear velocity
+            dx = full_velocity_signal[:3]
 
             if self.vmax is not None:
                 # implement velocity limiting
@@ -91,7 +106,6 @@ class controller:
                                          -self.lamb * scale * x_tilde)
             else:
                 # generate (x,y,z) force without velocity limiting)
-                # u_task[:3] = -self.kp * x_tilde + self.kv * (target_vel - dx)
                 u_task[:3] = -self.kp * x_tilde
 
         # # generate the orientation control signal in task space if a target
@@ -99,6 +113,7 @@ class controller:
         # if target_quat is not None and np.sum(mask[3:]) > 0:
         #     # get the quaternion describing orientation of ref_frame
         #     quat = self.robot_config.orientation(ref_frame, q)
+        #     w = full_velocity_signal[3:]  # angular velocity
         #
         #     # calculate the error between the two quaternions as
         #     # described in (Nakanishi et al, 2008)
@@ -116,41 +131,40 @@ class controller:
         # # apply mask
         # u_task *= mask
 
-        # incorporate task space inertia matrix
-        # self.training_signal = np.dot(J.T, np.dot(Mx, u_task))
-
-        # dJ = self.robot_config.dJ(ref_frame, q=q, dq=dq)
-        # dJ = dJ[:3]
-        # self.training_signal = np.dot(J.T, np.dot(Mx, (u_task - np.dot(dJ, dq))))
-        self.training_signal = np.dot(J.T, np.dot(Mx, u_task))
-
         # TODO: This is really awkward, but how else to get out
         # this signal for dynamics adaptation training?
+
+        # incorporate task space inertia matrix
+        self.training_signal = np.dot(J.T, np.dot(Mx, u_task))
+
         if self.vmax is None:
             self.training_signal -= np.dot(M, dq)
 
+        if self.use_dJ is True:
+            # add in estimate of current acceleration
+            dJ = self.robot_config.dJ(ref_frame, q=q, dq=dq)
+            # apply mask
+            dJ = dJ[:3]
+            self.training_signal -= np.dot(Mx, np.dot(dJ, dq))
+
         # cancel out effects of gravity
         u = self.training_signal - self.robot_config.g(q=q)
-        # cancel out centripetal and Coriolis effects
-        # u -= self.robot_config.C(q=q, dq=dq)
+
+        if self.use_C is True:
+            # add in estimation of centripetal and Coriolis effects
+            u -= self.robot_config.C(q=q, dq=dq)
 
         if self.null_control is True:
             # calculate the null space filter
-            # nkp = self.kp * .5  # NOTE: value for demo 3.2
-            nkp = self.kp * .1  # NOTE: normal gain value
+            nkp = self.kp * .1
             nkv = np.sqrt(nkp)
 
-            q_des = np.zeros(self.robot_config.num_joints, dtype='float32')
-            dq_des = np.zeros(self.robot_config.num_joints, dtype='float32')
+            # # calculated desired joint angle acceleration using rest angles
+            q_des = np.nan_to_num((self.robot_config.rest_angles - q + np.pi) %
+                                  (np.pi * 2) - np.pi)
+            self.dq_des[self.null_indices] = dq[self.null_indices]
 
-            # calculated desired joint angle acceleration using rest angles
-            for ii in range(1, self.robot_config.num_joints):
-                if self.robot_config.rest_angles[ii] is not None:
-                    q_des[ii] = (
-                        ((self.robot_config.rest_angles[ii] - q[ii]) + np.pi) %
-                        (np.pi * 2) - np.pi)
-                    dq_des[ii] = dq[ii]
-            u_null = np.dot(M, (nkp * q_des - nkv * dq_des))
+            u_null = np.dot(M, (nkp * q_des - nkv * self.dq_des))
 
             Jbar = np.dot(M_inv, np.dot(JEE.T, Mx))
             null_filter = (np.eye(self.robot_config.num_joints) -
