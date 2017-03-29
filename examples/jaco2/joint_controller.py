cc1e1d8fc0181affc3e37d7cbb16351ba35edb14
travis dewolf <travis.dewolf@gmail.com>
2017-03-29 11:30:00 -0400

    Working on pep8 cleanup

--- examples/jaco2/joint_controller.py
+++ examples/jaco2/joint_controller.py
@@ -5,7 +5,6 @@ the target angles once the final target is reached, and the arm
 has moved back to its default resting position.
 """
 import numpy as np
-import signal
 import sys
 import traceback
 
@@ -13,20 +12,18 @@ import abr_control
 
 # initialize our robot config for neural controllers
 robot_config = abr_control.arms.jaco2.config(
-    regenerate_functions=True, use_cython=True,
-    hand_attached=False)
+    use_cython=True, hand_attached=True)
 # instantiate the REACH controller for the jaco2 robot
-ctrlr = abr_control.controllers.joint(robot_config, kp=4, kv=2)
+ctrlr = abr_control.controllers.joint(robot_config, kp=20, kv=6)
 
-ctrlr.control(np.zeros(robot_config.num_joints),
-              np.zeros(robot_config.num_joints),
-              np.zeros(robot_config.num_joints))
+zeros = np.zeros(robot_config.num_joints)
+ctrlr.control(zeros, zeros, zeros)
 
 # create our interface for the jaco2
 interface = abr_control.interfaces.vrep(
     robot_config=robot_config, dt=.001)
 
-target_pos = np.array([2.0, 2.75, 3.45, 1.0, .5], dtype='float32')
+target_pos = np.array([2.0, 1.4, 1.8, 1.0, .5, .6], dtype='float32')
 target_vel = None
 
 # connect to the jaco
@@ -34,34 +31,11 @@ interface.connect()
 
 # set up arrays for tracking end-effector and target position
 q_track = []
-ctr = 0
 
-
-def on_exit(signal, frame):
-    """ A function for plotting the end-effector trajectory and error """
-    global q_track
-    q_track = np.array(q_track)
-
-    import matplotlib.pyplot as plt
-
-    plt.plot(q_track)
-    plt.plot(np.ones(q_track.shape) *
-             ((target_pos + np.pi) % (np.pi * 2) - np.pi),
-             'r--')
-    plt.tight_layout()
-    plt.show()
-    sys.exit()
-
-# call on_exit when ctrl-c is pressed
-signal.signal(signal.SIGINT, on_exit)
 
 try:
-    feedback = interface.get_feedback()
-    start = robot_config.Tx('EE', q=feedback['q'])
     while 1:
-        ctr += 1
         feedback = interface.get_feedback()
-        hand_xyz = robot_config.Tx('EE', q=feedback['q'])
 
         u = ctrlr.control(q=feedback['q'], dq=feedback['dq'],
                           target_pos=target_pos, target_vel=target_vel)
@@ -75,3 +49,16 @@ except:
 finally:
     # close the connection to the arm
     interface.disconnect()
+
+    q_track = np.array(q_track)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(q_track)
+    plt.gca().set_color_cycle(None)
+    plt.plot(np.ones(q_track.shape) *
+             ((target_pos + np.pi) % (np.pi * 2) - np.pi), '--')
+    plt.legend(range(6))
+    plt.tight_layout()
+    plt.show()
+    sys.exit()
