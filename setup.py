23e670ff9e0a9ca7cac5421b6c48f90b24c6bfae
travis dewolf <travis.dewolf@gmail.com>
2017-06-02 14:15:35 -0400

    making cython an explicit dependency, README

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 import io
 import runpy
 import numpy
@@ -21,36 +24,22 @@ version = runpy.run_path(os.path.join(root, 'abr_control',
                                       'version.py'))['version']
 long_description = read('README.rst')
 
-setup_pars = {
-    'name': "abr_control",
-    'version': version,
-    'author': "Applied Brain Research",
-    'author_email': "travis.dewolf@appliedbrainresearch.com",
-    'packages': find_packages(),
-    'include_package_data': True,
-    'scripts': [],
-    'url': "https://github.com/abr/control",
-    'license': "Free for non-commercial use",
-    'description': "A library for controlling and interfacing with robots.",
-    'long_description': long_description,
-    'install_requires': ["numpy", "cloudpickle", "sympy"]}
-
-try:
-    import cython
-    from distutils.core import setup
-    from distutils.extension import Extension
-    from Cython.Distutils import build_ext
-    # if cython is installed, also compile threelink arm simulation
-    setup_pars.update({
-        'cmdclass': {'build_ext': build_ext},
-        'ext_modules': [
-            Extension("abr_control/arms/threelink/arm_files/py3LinkArm",
-                sources=["abr_control/arms/threelink/arm_files/py3LinkArm.pyx"],
-                language="c++",
-                include_dirs=[numpy.get_include()])]})
-
-except ImportError:
-    print('Warning: Cython not installed, threelink arm simulation' +
-          'will not be compiled, and cannot be used.')
-
-setup(**setup_pars)
+setup(
+    name="abr_control",
+    version=version,
+    author="Applied Brain Research",
+    author_email="travis.dewolf@appliedbrainresearch.com",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[],
+    url="https://github.com/abr/control",
+    license="Free for non-commercial use",
+    description="A library for controlling and interfacing with robots.",
+    long_description=long_description,
+    install_requires=["numpy", "cloudpickle", "cython", "sympy"],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension("abr_control/arms/threelink/arm_files/py3LinkArm",
+            sources=["abr_control/arms/threelink/arm_files/py3LinkArm.pyx"],
+            language="c++",
+            include_dirs=[numpy.get_include()])])
