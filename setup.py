aead2fd08f689b803278ee573b45585ad65e0fa6
Trevor Bekolay <tbekolay@gmail.com>
2017-06-02 17:04:12 -0400

    Allow installing without reqs preinstalled
    With these changes, setuptools should properly install numpy
    and cython as part of the setup process and then compile the
    .pyx file.
    
    Also added two dependencies that weren't previously listed:
    matplotlib and scipy.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-# from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import io
 import runpy
-import numpy
 import os
 
-from setuptools import find_packages, setup
+from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 
 def read(*filenames, **kwargs):
@@ -20,9 +26,9 @@ def read(*filenames, **kwargs):
 
 
 root = os.path.dirname(os.path.realpath(__file__))
-version = runpy.run_path(os.path.join(root, 'abr_control',
-                                      'version.py'))['version']
-long_description = read('README.rst')
+version = runpy.run_path(
+    os.path.join(root, 'abr_control', 'version.py'))['version']
+setup_requires = ["setuptools>=18.0", "cython", "numpy"]
 
 setup(
     name="abr_control",
@@ -32,14 +38,20 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     scripts=[],
-    url="https://github.com/abr/control",
+    url="https://github.com/abr/abr_control",
     license="Free for non-commercial use",
     description="A library for controlling and interfacing with robots.",
-    long_description=long_description,
-    install_requires=["numpy", "cloudpickle", "cython", "sympy"],
+    long_description=read('README.rst'),
+    install_requires=setup_requires + [
+        "cloudpickle", "sympy", "nengo", "matplotlib", "scipy",
+    ],
+    setup_requires=setup_requires,
     cmdclass={'build_ext': build_ext},
     ext_modules=[
-        Extension("abr_control/arms/threelink/arm_files/py3LinkArm",
+        Extension(
+            "abr_control.arms.threelink.arm_files.py3LinkArm",
             sources=["abr_control/arms/threelink/arm_files/py3LinkArm.pyx"],
             language="c++",
-            include_dirs=[numpy.get_include()])])
+        )
+    ],
+)
