10871a852fcaca270a5a00c6b58a3f45a4e2c029
travis dewolf <travis.dewolf@gmail.com>
2017-06-05 17:01:15 -0400

    fix to paths for windows

--- abr_control/utils/paths.py
+++ abr_control/utils/paths.py
@@ -4,6 +4,7 @@ import sys
 """ Set the path based on the operating system"""
 
 if sys.platform.startswith('win'):
+    config_dir = os.path.expanduser(os.path.join("~", ".abr_control"))
     cache_dir = os.path.join(config_dir, "cache")
 else:
     cache_dir = os.path.expanduser(os.path.join("~", ".cache", "abr_control"))
