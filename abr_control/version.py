03f4e23d8da82bb2a4baa5e66e12cc2888c5fdee
travis dewolf <travis.dewolf@gmail.com>
2017-06-08 12:43:35 -0400

    updated version information

--- abr_control/version.py
+++ abr_control/version.py
@@ -1,6 +1,6 @@
 name = "abr_control"
 version_info = (0, 1, 0)  # (major, minor, patch)
-dev = True
+dev = False
 
 version = "{v}{dev}".format(v='.'.join(str(v) for v in version_info),
                             dev='.dev0' if dev else '')
