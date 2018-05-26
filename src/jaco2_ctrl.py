4a6266369687b3f3c0f7a6dd8675cf7e14db1b4a
niudong-ubuntu <1619249966@qq.com>
2018-05-26 15:14:19 +0800

    Add src file

---
+++ src/jaco2_ctrl.py
@@ -0,0 +1,32 @@
+import numpy as np
+from abr_control.arms import jaco2 as arm
+from abr_control.controllers import OSC
+from abr_control.interfaces import VREP
+
+# initialize our robot config for the ur5
+robot_config = arm.Config(use_cython=True, hand_attached=True)
+
+# instantiate controller
+ctrlr = OSC(robot_config, kp=200, vmax=0.5)
+
+# create our VREP interface
+interface = VREP(robot_config, dt=.05)
+interface.connect()
+
+target_xyz = np.array([1, 0.2, 0.5])
+# set the target object's position in VREP
+interface.set_xyz(name='target', xyz=target_xyz)
+
+count = 0.0
+while count < 1500:  # run for 1.5 simulated seconds
+    # get joint angle and velocity feedback
+    feedback = interface.get_feedback()
+    # calculate the control signal
+    u = ctrlr.generate(
+        q=feedback['q'],
+        dq=feedback['dq'],
+        target_pos=target_xyz)
+    # send forces into VREP, step the sim forward
+    interface.send_forces(u)
+    count += 1
+interface.disconnect()
