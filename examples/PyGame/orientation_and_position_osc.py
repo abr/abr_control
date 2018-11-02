24e480d309e3861b309ca1c3c6d90a31cf6d54c1
travis dewolf <travis.dewolf@gmail.com>
2018-11-02 11:47:19 -0400

    PyGame orientation example cleanup

--- examples/PyGame/orientation_and_position_osc.py
+++ examples/PyGame/orientation_and_position_osc.py
@@ -4,6 +4,7 @@ move the end-effector to the target, which can be moved by
 clicking on the background.
 """
 import numpy as np
+import pygame
 
 from abr_control.arms import threejoint as arm
 # from abr_control.arms import twojoint as arm
@@ -27,32 +28,41 @@ def on_click(self, mouse_x, mouse_y):
     self.target[1] = self.mouse_y
 
 
+def on_keypress(self, key):
+    if key == pygame.K_LEFT:
+        self.theta += np.pi / 10
+    if key == pygame.K_RIGHT:
+        self.theta -= np.pi / 10
+
+    R_theta = np.array([
+        [np.cos(interface.theta), -np.sin(interface.theta), 0],
+        [np.sin(interface.theta), np.cos(interface.theta), 0],
+        [0, 0, 1]])
+    R_target = np.dot(R_theta, R)
+    self.q_target = transformations.quaternion_from_matrix(R_target)
+    # from (Yuan, 1988), given r = [r1, r2, r3]
+    # r^x = [[0, -r3, r2], [r3, 0, -r1], [-r2, r1, 0]]
+    self.q_target_matrix = np.array([
+        [0, -self.q_target[2], self.q_target[1]],
+        [self.q_target[2], 0, -self.q_target[0]],
+        [-self.q_target[1], self.q_target[0], 0]])
+
+
 # create our interface
-interface = PyGame(robot_config, arm_sim, dt=.001, on_click=on_click)
+interface = PyGame(robot_config, arm_sim, dt=.001,
+                   on_click=on_click, on_keypress=on_keypress)
 interface.connect()
+
+# target orientation is initial orientation + theta rotation around z axis
+feedback = interface.get_feedback()
+R = robot_config.R('EE', feedback['q'])
+interface.theta = - 3 * np.pi / 4
+interface.on_keypress(interface, None)
 
 # create a target position
 feedback = interface.get_feedback()
 target_xyz = robot_config.Tx('EE', feedback['q']) - np.array([-.5, 1, 0])
-print(target_xyz)
 interface.set_target(target_xyz)
-
-# target orientation is initial orientation + theta rotation around z axis
-R = robot_config.R('EE', feedback['q'])
-theta = - 3 * np.pi / 4
-R_theta = np.array([
-    [np.cos(theta), -np.sin(theta), 0],
-    [np.sin(theta), np.cos(theta), 0],
-    [0, 0, 1]])
-R_target = np.dot(R_theta, R)
-q_target = transformations.quaternion_from_matrix(R_target)
-
-# from (Yuan, 1988), given r = [r1, r2, r3]
-# r^x = [[0, -r3, r2], [r3, 0, -r1], [-r2, r1, 0]]
-q_target_matrix = np.array([
-    [0, -q_target[2], q_target[1]],
-    [q_target[2], 0, -q_target[0]],
-    [-q_target[1], q_target[0], 0]])
 
 # set up lists for tracking data
 ee_path = []
@@ -60,9 +70,9 @@ target_path = []
 
 
 kw = 0.0  # gain on angular velocity difference
-kp = 10.0  # gain or position error
-ko = 10.0  # gain on orientation error
-kv = 10.0
+kp = 40.0  # gain or position error
+ko = 40.0  # gain on orientation error
+kv = 40.0
 w_d = 0.0  # target angular velocity
 dw_d = 0.0  # target angular acceleration
 try:
@@ -73,6 +83,7 @@ try:
 
     print('\nSimulation starting...\n')
     print('\nClick to move the target.\n')
+    print('\nPress left or right arrow to change target orientation angle.\n')
 
     count = 0
     while 1:
@@ -86,14 +97,16 @@ try:
         q_EE = transformations.quaternion_from_matrix(R)
 
         J = robot_config.J('EE', feedback['q'])
+        dx = np.dot(J, feedback['dq'])
 
         # calculate the difference between q_EE and q_target
         # from (Yuan, 1988)
         # dq = (w_d * [x, y, z] - w * [x_d, y_d, z_d] -
         #       [x_d, y_d, z_d]^x * [x, y, z])
-        w_tilde = - ko * (q_target[0] * q_EE[1:] - q_EE[0] * q_target[1:] -
-                np.dot(q_target_matrix, q_EE[1:]))
-        x_tilde = - kp * (hand_xyz - target_xyz)
+        w_tilde = -ko * (
+            interface.q_target[0] * q_EE[1:] - q_EE[0] * interface.q_target[1:] -
+            np.dot(interface.q_target_matrix, q_EE[1:]))
+        x_tilde = -kp * (hand_xyz - target_xyz)
 
         forces = np.hstack([x_tilde, w_tilde])[:, None]
 
--- examples/PyGame/orientation_osc.py
+++ examples/PyGame/orientation_osc.py
@@ -4,11 +4,11 @@ move the end-effector to the target, which can be moved by
 clicking on the background.
 """
 import numpy as np
+import pygame
 
 from abr_control.arms import threejoint as arm
 # from abr_control.arms import twojoint as arm
 from abr_control.interfaces import PyGame
-from abr_control.controllers import OSC
 from abr_control.utils import transformations
 
 
@@ -17,31 +17,32 @@ robot_config = arm.Config(use_cython=True)
 # create our arm simulation
 arm_sim = arm.ArmSim(robot_config)
 
-# create an operational space controller
-ctrlr = OSC(robot_config, kp=20, vmax=None,
-            use_C=True, use_g=False)
+def on_keypress(self, key):
+    if key == pygame.K_LEFT:
+        self.theta += np.pi / 10
+    if key == pygame.K_RIGHT:
+        self.theta -= np.pi / 10
+
+    R_theta = np.array([
+        [np.cos(interface.theta), -np.sin(interface.theta), 0],
+        [np.sin(interface.theta), np.cos(interface.theta), 0],
+        [0, 0, 1]])
+    R_target = np.dot(R_theta, R)
+    self.q_target = transformations.quaternion_from_matrix(R_target)
+    # from (Yuan, 1988), given r = [r1, r2, r3]
+    # r^x = [[0, -r3, r2], [r3, 0, -r1], [-r2, r1, 0]]
+    self.q_target_matrix = np.array([
+        [0, -self.q_target[2], self.q_target[1]],
+        [self.q_target[2], 0, -self.q_target[0]],
+        [-self.q_target[1], self.q_target[0], 0]])
+
 
 # create our interface
-interface = PyGame(robot_config, arm_sim, dt=.001)
+interface = PyGame(robot_config, arm_sim, dt=.001,
+                   on_keypress=on_keypress)
 interface.connect()
-
-# target orientation is initial orientation + theta rotation around z axis
-feedback = interface.get_feedback()
-R = robot_config.R('EE', feedback['q'])
-theta = np.pi / 4
-R_theta = np.array([
-    [np.cos(theta), -np.sin(theta), 0],
-    [np.sin(theta), np.cos(theta), 0],
-    [0, 0, 1]])
-R_target = np.dot(R_theta, R)
-q_target = transformations.quaternion_from_matrix(R_target)
-
-# from (Yuan, 1988), given r = [r1, r2, r3]
-# r^x = [[0, -r3, r2], [r3, 0, -r1], [-r2, r1, 0]]
-q_target_matrix = np.array([
-    [0, -q_target[2], q_target[1]],
-    [q_target[2], 0, -q_target[0]],
-    [-q_target[1], q_target[0], 0]])
+interface.theta = - 3 * np.pi / 4
+interface.on_keypress(interface, None)
 
 # set up lists for tracking data
 ee_path = []
@@ -49,18 +50,17 @@ target_path = []
 
 
 kw = 0.0  # gain on angular velocity difference
-ko = 10.0  # gain on orientation error
-kv = 10.0
+ko = 40.0  # gain on orientation error
+kv = 40.0
 w_d = 0.0  # target angular velocity
 dw_d = 0.0  # target angular acceleration
 try:
     # run ctrl.generate once to load all functions
     zeros = np.zeros(robot_config.N_JOINTS)
-    ctrlr.generate(q=zeros, dq=zeros, target_pos=np.zeros(3))
     robot_config.R('EE', q=zeros)
 
     print('\nSimulation starting...\n')
-    print('\nClick to move the target.\n')
+    print('\nPress left or right arrow to change target orientation angle.\n')
 
     count = 0
     while 1:
@@ -74,17 +74,15 @@ try:
         q_EE = transformations.quaternion_from_matrix(R)
 
         J = robot_config.J('EE', feedback['q'])[3:]
-        J_inv = np.linalg.pinv(J)
         w = np.dot(J, feedback['q'])  # calculate the task space velocities
-        # dx = np.dot(J, feedback['q'])  # calculate the task space velocities
-        # w = dx[3:]  # isolate the angular velocities
 
         # calculate the difference between q_EE and q_target
         # from (Yuan, 1988)
         # dq = (w_d * [x, y, z] - w * [x_d, y_d, z_d] -
         #       [x_d, y_d, z_d]^x * [x, y, z])
-        w_tilde = - ko * (q_target[0] * q_EE[1:] - q_EE[0] * q_target[1:] -
-                          np.dot(q_target_matrix, q_EE[1:]))
+        w_tilde = - ko * (
+            interface.q_target[0] * q_EE[1:] - q_EE[0] * interface.q_target[1:] -
+            np.dot(interface.q_target_matrix, q_EE[1:]))
 
         M = robot_config.M(feedback['q'])
         M_inv = np.linalg.inv(M)
