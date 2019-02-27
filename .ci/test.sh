5b9e52e8092694018fe7273dd488530e1d223b2f
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-02-27 12:19:26 -0400

    Add Travis-CI skeleton

---
+++ .ci/test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+if [[ ! -e .ci/common.sh || ! -e abr_control ]]; then
+    echo "Run this script from the root directory of this repository"
+    exit 1
+fi
+source .ci/common.sh
+
+# This script runs the test suite
+
+NAME=$0
+COMMAND=$1
+
+if [[ "$COMMAND" == "install" ]]; then
+    exe pip install -e .
+elif [[ "$COMMAND" == "script" ]]; then
+    exe python -c "import abr_control"
+elif [[ -z "$COMMAND" ]]; then
+    echo "$NAME requires a command like 'install' or 'script'"
+else
+    echo "$NAME does not define $COMMAND"
+fi
+
+exit "$STATUS"
