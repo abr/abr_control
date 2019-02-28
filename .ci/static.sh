d81b41b1a61e3486dac5376e4b206f5cf243e2d1
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-02-28 12:34:15 -0400

    Add static style checking to CI

---
+++ .ci/static.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+shopt -s globstar
+if [[ ! -e .ci/common.sh || ! -e abr_control ]]; then
+    echo "Run this script from the root directory of this repository"
+    exit 1
+fi
+source .ci/common.sh
+
+# This script runs the static style checks
+
+NAME=$0
+COMMAND=$1
+
+if [[ "$COMMAND" == "install" ]]; then
+    # astroid==2.2 causes an error when running pylint
+    exe pip install codespell pylint gitlint "astroid<2.2.0"
+elif [[ "$COMMAND" == "script" ]]; then
+    exe pylint abr_control --rcfile=setup.cfg
+    exe codespell -q 3
+    exe shellcheck -e SC2087 .ci/*.sh
+    # undo single-branch cloning
+    git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+    git fetch origin master
+    N_COMMITS=$(git rev-list --count HEAD ^origin/master)
+    for ((i=0; i<N_COMMITS; i++)) do
+        git log -n 1 --skip $i --pretty=%B | grep -v '^Co-authored-by:' | exe gitlint -vvv
+    done
+elif [[ -z "$COMMAND" ]]; then
+    echo "$NAME requires a command like 'install' or 'script'"
+else
+    echo "$NAME does not define $COMMAND"
+fi
+
+exit "$STATUS"
