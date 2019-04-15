8be2daa2182e1cd414156201111426a443f18eba
travis dewolf <travis.dewolf@gmail.com>
2019-04-15 17:39:16 -0400

    Refactoring of dynamics adaptation
    - removed support of multiple backends
    - removed helper functions for running data collection trials
    - removed redis support
    - removed initialization to a function support
    
    - loading weights from file still needs to be tested

--- examples/PyGame/dynamics_adaptation_osc.py
+++ examples/PyGame/dynamics_adaptation_osc.py
@@ -31,7 +31,7 @@ ctrlr = OSC(robot_config, kp=50, null_controllers=[damping])
 adapt = signals.DynamicsAdaptation(
     n_input=robot_config.N_JOINTS,
     n_output=robot_config.N_JOINTS,
-    pes_learning_rate=1e-4, backend='nengo')
+    pes_learning_rate=1e-4)
 
 def on_click(self, mouse_x, mouse_y):
     self.target[0] = self.mouse_x
