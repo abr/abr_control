1020e8acac8c9cf887293969c6ed5739c434d516
travis dewolf <travis.dewolf@gmail.com>
2019-05-10 14:48:59 -0400

    Added test for scaling, spherical fixes
    - also changed MEANS and VARIANCES to means and variances
    - adjusted parameters on examples to run smoother
    - TODO: sliding control adaptation isn't working great still

---
+++ abr_control/controllers/signals/tests/test_dynamics_adaptation.py
@@ -0,0 +1,41 @@
+import pytest
+
+import numpy as np
+
+from abr_control.arms import ur5
+from abr_control.controllers.signals import DynamicsAdaptation
+
+def test_scaling():
+    robot_config = ur5.Config(use_cython=True)
+
+    # looking at data centered around 50 with range of 25 each way
+    means = np.ones(robot_config.N_JOINTS) * 50
+    variances = np.ones(robot_config.N_JOINTS) * 25
+
+    for ii in range(50):
+        # test without spherical conversion
+        adapt = DynamicsAdaptation(
+            robot_config.N_JOINTS,
+            robot_config.N_JOINTS,
+            means=means,
+            variances=variances,
+            spherical=False,
+            )
+
+        unscaled = np.random.random(robot_config.N_JOINTS) * 50 + 25
+        scaled = adapt.scale_inputs(unscaled)
+        assert np.all(-1 < scaled) and np.all(scaled < 1)
+
+
+        # test spherical conversion
+        adapt = DynamicsAdaptation(
+            robot_config.N_JOINTS,
+            robot_config.N_JOINTS,
+            means=means,
+            variances=variances,
+            spherical=True,
+            )
+
+        unscaled = np.random.random(robot_config.N_JOINTS) * 50 + 25
+        scaled = adapt.scale_inputs(unscaled)
+        assert np.linalg.norm(scaled) - 1 < 1e-5
