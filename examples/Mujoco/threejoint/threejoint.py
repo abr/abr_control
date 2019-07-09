2f98ecfb92d8b26f51a60a0fe738602b4af8469c
p3jawors <p3jawors@gmail.com>
2019-07-09 15:42:50 -0400

    Code cleanup and import fixes, centralize xmls
    - fixed non-jaco examples to import mujoco interface and config
      directly
    - clean up of xml and python files for jaco partial link arms
    - added jaco mujoco example with path planner
    
    - config.xml files are now located in
      abr_control/arms/armName/armName.xml
    - to set the arm config in mujoco_config you pass a string with the
      armName
    - if alternate arm models are created, place them in the armName
      directory, but name the xml config as armName_alternate_thing
      See note in mujoco_config.py for more details
    
    - added mock interfaces for vrep interface, mujoco interface, and
    mujoco_config for testing outside of the repo
    - mujoco offset not implemented exception allows for all zeros offset
    
    - self.N_JOINTS and self.START_ANGLES are now pulled from the custom
    tags in the arm xml files and are accessible through the mujoco_config
    
    - added option to turn off visualization for mujoco

--- examples/Mujoco/threejoint/threejoint.py
+++ examples/Mujoco/threejoint/threejoint.py
@@ -1,3 +1,12 @@
+"""
+Minimal example using the mujoco interface with a three link arm
+reaching to a target with three degrees of freedom
+
+To plot the joint angles call the script with True
+passed through sys.argv
+    python threelink.py True
+"""
+
 import glfw
 import sys
 import numpy as np
@@ -8,18 +17,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 from abr_control.controllers import OSC, Damping, RestingConfig
-from abr_control.interfaces import Mujoco
-from abr_control.arms import MujocoConfig
+from abr_control.interfaces.mujoco import Mujoco
+from abr_control.arms.mujoco_config import MujocoConfig
 
 
 if len(sys.argv) > 1:
     show_plot = sys.argv[1]
 else:
     show_plot = False
-# NOTE: the meshes file from MuJoCo_Unity_UR5/xmls needs to be in the
-# Mujoco examples folder for this to load properly
-model_filename = 'threelink.xml'
-q_init = np.array([0, 0, 0])#, -np.pi/2, np.pi/2])
+model_filename = 'threejoint'
+q_init = np.array([0, 0, 0])
 
 robot_config = MujocoConfig(model_filename)
 
@@ -38,10 +45,8 @@ interface.set_mocap_xyz('target', target[:3])
 interface.set_mocap_xyz('hand', np.array([.2, .4, 1]))
 
 q_track = []
-# ee_track = []
-# target_track = []
-
 count = 0
+
 link_name = 'EE'
 try:
     while True:
@@ -71,16 +76,12 @@ try:
             interface.set_mocap_xyz('target', target[:3])
 
         q_track.append(feedback['q'])
-        # ee_track.append(np.copy(hand_xyz))
-        # target_track.append(interface.get_mocap_xyz(link_name))
         count += 1
 
 finally:
     interface.disconnect()
 
     q_track = np.asarray(q_track)
-    # ee_track = np.array(ee_track)
-    # target_track = np.array(target_track)
 
     if show_plot:
         plt.figure(figsize=(30, 30))
@@ -88,17 +89,5 @@ finally:
         plt.plot(q_track)
         plt.ylabel('Joint Angles [rad]')
         plt.legend(['0', '1'])
-        # plt.subplot(2, 1, 1, projection='3d')
-        # plt.plot(ee_track[:, 0], ee_track[:, 1], ee_track[:, 2])
-        # plt.plot(target_track[:, 0], target_track[:, 1], target_track[:, 2], 'rx', mew=3, lw=2)
-        # plt.gca().set_xlim([-1, 1])
-        # plt.gca().set_ylim([-1, 1])
-        # plt.gca().set_zlim([0, 1])
-        # plt.gca().set_aspect('equal')
-        #
-        # plt.subplot(2, 1, 2)
-        # plt.plot(ee_track, lw=2)
-        # plt.gca().set_prop_cycle(None)
-        # plt.plot(target_track, '--', lw=2)
 
         plt.show()
