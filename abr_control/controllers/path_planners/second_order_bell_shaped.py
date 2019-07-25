MJ example fix and bell-shaped PP time scaling
- fixed issue with time scaling in the bell-shaped path planner, will
  actually see paths lasting n timesteps take n timesteps to reach
  the target

- tweaks and fixups to the examples
- changing starting positions
- randomizing target locations
- prettying up the movements
- changing time step from 0.005 to 0.001

Fixed a few examples

- dynamics_adaptation mujoco example adapts properly
- pos and orientation planner example now autogenerate targets and
  orientations every n timesteps
- added printout to position_and_orientation_control_osc that specifies
  the dof being controlled, otherwise it looks like the arm is missing
  the target

Fixed plotting in orientation tracking

- target was switched to `target_orientation` object so you can see
  the orientation as well, but the target orientation tracking was
  still referencing the `target` object
- changed plotting to only plot the controlled dof
- plot updates based on what you set ctrlr_dof to

abr_control/controllers/path_planners/second_order_bell_shaped.py
@@ -54,7 +54,10 @@ class BellShaped(PathPlanner):
         # want to follow the same trajectory in (x, y, z)
         y_des = np.vstack([y_des, y_des, y_des])
 
-        self.dmps = pydmps.DMPs_discrete(n_dmps=3, n_bfs=50, dt=.01)
+        # we can control the DMP rollout speed with the time step size
+        # the DMP will reach the target in 1s of sim time
+        dt = 1 / n_timesteps
+        self.dmps = pydmps.DMPs_discrete(n_dmps=3, n_bfs=50, dt=dt)
         self.dmps.imitate_path(y_des)
 
 

examples/Mujoco/dynamics_adaptation_osc.py
@@ -28,24 +28,29 @@ ctrlr = OSC(
 
 # create our adaptive controller
 adapt = signals.DynamicsAdaptation(
-    n_neurons=1000,
+    n_neurons=5000,
     n_ensembles=5,
-    n_input=2,  # we apply adaptation on the most heavily stressed joints
-    n_output=2,
+    n_input=10,  # we apply adaptation on the most heavily stressed joints
+    n_output=5,
     pes_learning_rate=5e-5,
-    intercepts_bounds=[-0.6, -0.2],
-    intercepts_mode=-0.2,
-    means=[3.14, 3.14],
-    variances=[1.57, 1.57])
+    intercepts_bounds=[-0.3, 0.1],
+    intercepts_mode=-0.1,
+    means=[0.12, 2.14, 1.87, 4.32, 0.59,
+        0.12, -0.38, -0.42, -0.29, 0.36],
+    variances=[0.08, 0.6, 0.7, 0.3, 0.6,
+        0.08, 1.4, 1.6, 0.7, 1.2],
+    spherical=True)
 
 # create our Mujoco interface
-interface = Mujoco(robot_config, dt=.005)
+interface = Mujoco(robot_config, dt=.001)
 interface.connect()
+interface.send_target_angles(robot_config.START_ANGLES)
 
 # set up lists for tracking data
 ee_track = []
 target_track = []
-
+q_track = []
+dq_track = []
 
 try:
     # get the end-effector's initial position
@@ -78,11 +83,11 @@ try:
             )
 
         u_adapt = np.zeros(robot_config.N_JOINTS)
-        u_adapt[1:3] = adapt.generate(
-            input_signal=np.array(
-                [feedback['q'][1], feedback['q'][2]]),
+        u_adapt[:5] = adapt.generate(
+            input_signal=np.hstack(
+                (feedback['q'][:5], feedback['dq'][:5])),
             training_signal=np.array(
-                [ctrlr.training_signal[1], ctrlr.training_signal[2]]))
+                ctrlr.training_signal[:5]))
         u += u_adapt
 
         # add an additional force for the controller to adapt to
@@ -97,6 +102,8 @@ try:
         # track data
         ee_track.append(np.copy(ee_xyz))
         target_track.append(np.copy(target[:3]))
+        q_track.append(np.copy(feedback['q']))
+        dq_track.append(np.copy(feedback['dq']))
 
         count += 1
 
@@ -109,6 +116,8 @@ finally:
 
     ee_track = np.array(ee_track)
     target_track = np.array(target_track)
+    q_track = np.asarray(q_track)
+    dq_track = np.asarray(dq_track)
 
     if ee_track.shape[0] > 0:
         # plot distance from target and 3D trajectory
@@ -116,6 +125,18 @@ finally:
         from mpl_toolkits.mplot3d import axes3d  # pylint: disable=W0611
 
         fig = plt.figure(figsize=(8,12))
+
+        # for ii in range(6):
+        #     a = fig.add_subplot(6,2,ii*2+1)
+        #     a.plot(q_track[:, ii], label=np.mean(q_track[:, ii]))
+        #     a.set_title('q%i'%ii)
+        #     a.legend()
+        #     a2 = fig.add_subplot(6,2,ii*2+2)
+        #     a2.plot(dq_track[:, ii], label=np.mean(dq_track[:, ii]))
+        #     a2.set_title('dq%i'%ii)
+        #     a2.legend()
+        # plt.show()
+
         ax1 = fig.add_subplot(211)
         ax1.set_ylabel('Distance (m)')
         ax1.set_xlabel('Time (ms)')

examples/Mujoco/joint.py
@@ -16,15 +16,16 @@ from abr_control.controllers import Joint
 robot_config = arm('jaco2')
 
 # instantiate the REACH controller for the jaco2 robot
-ctrlr = Joint(robot_config, kp=50)
+ctrlr = Joint(robot_config, kp=20, kv=10)
 
 # create interface and connect
-interface = Mujoco(robot_config=robot_config, dt=.005)
+interface = Mujoco(robot_config=robot_config, dt=.001)
 interface.connect()
+# interface.send_target_angles(robot_config.START_ANGLES)
 
 # make the target an offset of the current configuration
 feedback = interface.get_feedback()
-target = feedback['q'] + np.ones(robot_config.N_JOINTS) * .3
+target = feedback['q'] + np.random.random(robot_config.N_JOINTS) * 2 - 1
 
 # set up arrays for tracking end-effector and target position
 q_track = []

examples/Mujoco/path_and_orientation_planner_bell_shaped.py
@@ -33,28 +33,36 @@ ctrlr = OSC(
     ctrlr_dof = [True, True, True, True, True, True])
 
 # create our interface
-interface = Mujoco(robot_config, dt=.005)
+interface = Mujoco(robot_config, dt=.001)
 interface.connect()
-
-# pregenerate our path and orientation planners
-n_timesteps = 3000
-traj_planner = path_planners.BellShaped(
-    error_scale=0.1, n_timesteps=n_timesteps)
 
 feedback = interface.get_feedback()
 hand_xyz = robot_config.Tx('EE', feedback['q'])
-starting_orientation = robot_config.quaternion('EE', feedback['q'])
 
-target_orientation = np.random.random(3)
-target_orientation /= np.linalg.norm(target_orientation)
-# convert our orientation to a quaternion
-target_orientation = [0] + list(target_orientation)
-target_position = [0.4, -0.3, 0.5]
+def get_target(robot_config):
+    # pregenerate our path and orientation planners
+    n_timesteps = 2000
+    traj_planner = path_planners.BellShaped(
+        error_scale=0.01, n_timesteps=n_timesteps)
 
-traj_planner.generate_path(
-    position=hand_xyz, target_pos=target_position)
-_, orientation_planner = traj_planner.generate_orientation_path(
-    orientation=starting_orientation, target_orientation=target_orientation)
+    starting_orientation = robot_config.quaternion('EE', feedback['q'])
+
+    target_orientation = np.random.random(3)
+    target_orientation /= np.linalg.norm(target_orientation)
+    # convert our orientation to a quaternion
+    target_orientation = [0] + list(target_orientation)
+
+    #target_position = [0.4, -0.3, 0.5]
+    mag = 0.6
+    target_position = np.random.random(3)*0.5
+    target_position = target_position / np.linalg.norm(target_position) * mag
+
+    traj_planner.generate_path(
+        position=hand_xyz, target_pos=target_position)
+    _, orientation_planner = traj_planner.generate_orientation_path(
+        orientation=starting_orientation, target_orientation=target_orientation)
+
+    return traj_planner, orientation_planner, target_position, target_orientation
 
 # set up lists for tracking data
 ee_track = []
@@ -65,8 +73,6 @@ target_angles_track = []
 
 try:
     count = 0
-    interface.set_mocap_xyz('target_orientation', target_position)
-    interface.set_mocap_orientation('target_orientation', target_orientation)
 
     print('\nSimulation starting...\n')
     while 1:
@@ -76,8 +82,12 @@ try:
         # get arm feedback
         feedback = interface.get_feedback()
         hand_xyz = robot_config.Tx('EE', feedback['q'])
+        if count % 3000 == 0:
+            traj_planner, orientation_planner, target_position, target_orientation = get_target(robot_config)
+            interface.set_mocap_xyz('target_orientation', target_position)
+            interface.set_mocap_orientation('target_orientation', target_orientation)
 
-        pos, vel = traj_planner.next()[:3]
+        pos, vel = traj_planner.next()
         orient = orientation_planner.next()
         target = np.hstack([pos, orient])
 

examples/Mujoco/position_and_orientation_control_osc.py
@@ -13,23 +13,29 @@ from abr_control.interfaces.mujoco import Mujoco
 from abr_control.utils import transformations
 from abr_control.controllers import OSC, Damping
 
-
+print('******************************')
+print('* DOF Controlled: x, a, b ,g *')
+print('******************************')
 # initialize our robot config
 robot_config = arm('jaco2')
+ctrlr_dof = [False, True, False, True, True, True]
+dof_labels = ['x', 'y', 'z', 'a', 'b', 'g']
 
 # damp the movements of the arm
 damping = Damping(robot_config, kv=10)
 # create opreational space controller
 ctrlr = OSC(
     robot_config,
-    kp=200,
+    kp=30,
+    kv=20,
+    ko=180,
     null_controllers=[damping],
     vmax=[10, 10],  # [m/s, rad/s]
     # control (x, alpha, beta, gamma) out of [x, y, z, alpha, beta, gamma]
-    ctrlr_dof=[True, False, False, True, True, True])
+    ctrlr_dof=ctrlr_dof)
 
 # create our interface
-interface = Mujoco(robot_config, dt=.005)
+interface = Mujoco(robot_config, dt=.001)
 interface.connect()
 
 target_orientation = np.random.random(4)
@@ -75,7 +81,7 @@ try:
         ee_angles_track.append(transformations.euler_from_matrix(
             robot_config.R('EE', feedback['q']), axes='rxyz'))
         target_track.append(np.copy(target[:3]))
-        target_angles_track.append(interface.get_mocap_orientation('target'))
+        target_angles_track.append(np.copy(target[3:]))
         count += 1
 
 finally:
@@ -97,14 +103,20 @@ finally:
         fig = plt.figure(figsize=(8,12))
         ax1 = fig.add_subplot(311)
         ax1.set_ylabel('3D position (m)')
-        ax1.plot(ee_track)
-        ax1.plot(target_track, '--')
+        for ii, controlled_dof in enumerate(ctrlr_dof[:3]):
+            if controlled_dof:
+                ax1.plot(ee_track[:, ii], label=dof_labels[ii])
+                ax1.plot(target_track[:, ii], '--')
+        ax1.legend()
 
         ax2 = fig.add_subplot(312)
-        ax2.plot(ee_angles_track)
-        ax2.plot(target_angles_track, '--')
+        for ii, controlled_dof in enumerate(ctrlr_dof[3:]):
+            if controlled_dof:
+                ax2.plot(ee_angles_track[:, ii], label=dof_labels[ii+3])
+                ax2.plot(target_angles_track[:, ii], '--')
         ax2.set_ylabel('3D orientation (rad)')
         ax2.set_xlabel('Time (s)')
+        ax2.legend()
 
         ax3 = fig.add_subplot(313, projection='3d')
         ax3.set_title('End-Effector Trajectory')