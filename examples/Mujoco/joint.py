1462d3a7f5e4f69d2dbe4b6ad34383cb50bd903e
p3jawors <p3jawors@gmail.com>
2019-07-25 15:23:16 -0400

    Added Vrep examples into Mujoco
    - missing the sliding controller because dJ is not implemented in
      mujoco_config.py
    - missing aboid_obstacles because mujoco_config.Tx does not currently
      support joint cartesian locations
    - tested and all work, including a graceful exit and plotting
    
    - fixed a path planner issue in vrep example
    
    - all examples tested and run
    
    - NOTE: it appears that the Pygame avoid_obstacles example does not
      work as it passes through the obstacle

---
+++ examples/Mujoco/joint.py
@@ -0,0 +1,75 @@
+"""
+A basic script for connecting and moving the arm to a target
+configuration in joint space, offset from its starting position.
+The simulation simulates 1500 time steps and then plots the results.
+"""
+import numpy as np
+import traceback
+import glfw
+
+from abr_control.arms.mujoco_config import MujocoConfig as arm
+from abr_control.interfaces.mujoco import Mujoco
+from abr_control.controllers import Joint
+
+
+# initialize our robot config
+robot_config = arm('jaco2')
+
+# instantiate the REACH controller for the jaco2 robot
+ctrlr = Joint(robot_config, kp=50)
+
+# create interface and connect
+interface = Mujoco(robot_config=robot_config, dt=.005)
+interface.connect()
+
+# make the target an offset of the current configuration
+feedback = interface.get_feedback()
+target = feedback['q'] + np.ones(robot_config.N_JOINTS) * .3
+
+# set up arrays for tracking end-effector and target position
+q_track = []
+
+
+try:
+    count = 0
+    print('\nSimulation starting...\n')
+    while count < 1500:
+        if interface.viewer.exit:
+            glfw.destroy_window(interface.viewer.window)
+            break
+        # get joint angle and velocity feedback
+        feedback = interface.get_feedback()
+
+        # calculate the control signal
+        u = ctrlr.generate(
+            q=feedback['q'],
+            dq=feedback['dq'],
+            target=target,
+            )
+
+        # send forces into Mujoco, step the sim forward
+        interface.send_forces(u)
+
+        # track joint angles
+        q_track.append(np.copy(feedback['q']))
+        count += 1
+
+except:
+    print(traceback.format_exc())
+
+finally:
+    # close the connection to the arm
+    interface.disconnect()
+
+    print('Simulation terminated...')
+
+    q_track = np.array(q_track)
+    if q_track.shape[0] > 0:
+        import matplotlib.pyplot as plt
+        plt.plot((q_track + np.pi) % (np.pi * 2) - np.pi)
+        plt.gca().set_prop_cycle(None)
+        plt.plot(np.ones(q_track.shape) *
+                ((target + np.pi) % (np.pi * 2) - np.pi), '--')
+        plt.legend(range(robot_config.N_LINKS))
+        plt.tight_layout()
+        plt.show()
---
+++ examples/Mujoco/position_and_orientation_control_osc.py
@@ -0,0 +1,115 @@
+"""
+Running operational space control using Mujoco. The controller will
+move the end-effector to the target object's X position and orientation.
+
+The cartesian direction being controlled is set in the first three booleans
+of the ctrlr_dof parameter
+"""
+import numpy as np
+import glfw
+
+from abr_control.arms.mujoco_config import MujocoConfig as arm
+from abr_control.interfaces.mujoco import Mujoco
+from abr_control.utils import transformations
+from abr_control.controllers import OSC, Damping
+
+
+# initialize our robot config
+robot_config = arm('jaco2')
+
+# damp the movements of the arm
+damping = Damping(robot_config, kv=10)
+# create opreational space controller
+ctrlr = OSC(
+    robot_config,
+    kp=200,
+    null_controllers=[damping],
+    vmax=[10, 10],  # [m/s, rad/s]
+    # control (x, alpha, beta, gamma) out of [x, y, z, alpha, beta, gamma]
+    ctrlr_dof=[True, False, False, True, True, True])
+
+# create our interface
+interface = Mujoco(robot_config, dt=.005)
+interface.connect()
+
+target_orientation = np.random.random(4)
+target_orientation /= np.linalg.norm(target_orientation)
+target_xyz = np.array([0.3, 0.3, 0.5])
+
+# set up lists for tracking data
+ee_track = []
+ee_angles_track = []
+target_track = []
+target_angles_track = []
+
+
+try:
+    count = 0
+    print('\nSimulation starting...\n')
+    interface.set_mocap_xyz('target_orientation', target_xyz)
+    interface.set_mocap_orientation('target_orientation', target_orientation)
+    while 1:
+        if interface.viewer.exit:
+            glfw.destroy_window(interface.viewer.window)
+            break
+        # get arm feedback
+        feedback = interface.get_feedback()
+        hand_xyz = robot_config.Tx('EE', feedback['q'])
+
+        target = np.hstack([
+            interface.get_mocap_xyz('target_orientation'),
+            transformations.euler_from_quaternion(
+                interface.get_mocap_orientation('target_orientation'), 'rxyz')])
+
+        u = ctrlr.generate(
+            q=feedback['q'],
+            dq=feedback['dq'],
+            target=target,
+            )
+
+        # apply the control signal, step the sim forward
+        interface.send_forces(u)
+
+        # track data
+        ee_track.append(np.copy(hand_xyz))
+        ee_angles_track.append(transformations.euler_from_matrix(
+            robot_config.R('EE', feedback['q']), axes='rxyz'))
+        target_track.append(np.copy(target[:3]))
+        target_angles_track.append(interface.get_mocap_orientation('target'))
+        count += 1
+
+finally:
+    # stop and reset the simulation
+    interface.disconnect()
+
+    print('Simulation terminated...')
+
+    ee_track = np.array(ee_track)
+    ee_angles_track = np.array(ee_angles_track)
+    target_track = np.array(target_track)
+    target_angles_track = np.array(target_angles_track)
+
+    if ee_track.shape[0] > 0:
+        # plot distance from target and 3D trajectory
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import axes3d  # pylint: disable=W0611
+
+        fig = plt.figure(figsize=(8,12))
+        ax1 = fig.add_subplot(311)
+        ax1.set_ylabel('3D position (m)')
+        ax1.plot(ee_track)
+        ax1.plot(target_track, '--')
+
+        ax2 = fig.add_subplot(312)
+        ax2.plot(ee_angles_track)
+        ax2.plot(target_angles_track, '--')
+        ax2.set_ylabel('3D orientation (rad)')
+        ax2.set_xlabel('Time (s)')
+
+        ax3 = fig.add_subplot(313, projection='3d')
+        ax3.set_title('End-Effector Trajectory')
+        ax3.plot(ee_track[:, 0], ee_track[:, 1], ee_track[:, 2], label='ee_xyz')
+        ax3.scatter(target_track[0, 0], target_track[0, 1], target_track[0, 2],
+                    label='target', c='g')
+        ax3.legend()
+        plt.show()
