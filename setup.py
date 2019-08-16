5d2e906b4dc0ae4215719bca7fde02c7ef8bc56c
p3jawors <p3jawors@gmail.com>
2019-08-16 12:32:54 -0400

    Bug fix for pade and nengolib
    - bug related to nengolib and versions of scipy>=1.2.0
    - removed note to pip install cython and scipy in README and added the
      install to the setup

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ version = runpy.run_path(
     os.path.join(root, 'abr_control', 'version.py'))['version']
 
 setup_requires = [
+    "scipy==1.1.0",
+    "cython<= 0.29.13",
     "setuptools>=18.0",
     "cython>=0.29.0",
     "numpy>=1.16.0"]
