3ac3605fd5c866bef8e8b9e5109233a9c352d9c5
p3jawors <p3jawors@gmail.com>
2019-11-05 15:55:02 -0500

    Grasping without slipping with hard contacts
    - external access to interface sim added
    - removed py dependency
    - changed start angles
    - higher torsional and sliding friction
    - thumb had different damping, now all the same

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ version = runpy.run_path(
     os.path.join(root, 'abr_control', 'version.py'))['version']
 
 setup_requires = [
-    "scipy==1.1.0",
+    "scipy>=1.1.0",
     "cython<= 0.29.13",
     "setuptools>=18.0",
     "cython>=0.29.0",
